e3489ceac2b32113a5467491f13e2cdb6907e1b9
pablo gonzalez <pablo949504563@gmail.com>
2020-07-18 17:56:31 -0300

    A brench loop-2 foi alterada novamente

--- src/main/java/loop/Loop2.java
+++ src/main/java/loop/Loop2.java
@@ -3,7 +3,7 @@ package loop;
 public class Loop2 {
 
 	public static void main(String[] args) {
-		System.out.println("Alteração das informações da branch");
+		System.out.println("Alteração das informações da branch-loop2");
 
 	}
 
